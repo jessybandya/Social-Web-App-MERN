f60d7c0d8f0e51c61b2509109e511d0f8b5842f3
unknown <jessy.bandya5@gmail.com>
2021-07-25 16:29:58 -0700

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar:{
+        type: String,
+        default: 'https://user-images.githubusercontent.com/11250/39013954-f5091c3a-43e6-11e8-9cac-37cf8e8c8e4e.jpg'
+    },
+    role: {type: String, default: 'user'},
+    gender: {type: String, default: 'male'},
+    mobile: {type: String, default: ''},
+    address: {type: String, default: ''},
+    story: {
+        type: String, 
+        default: '',
+        maxlength: 200
+    },
+    website: {type: String, default: ''},
+    followers: [{type: mongoose.Types.ObjectId, ref: 'user'}],
+    following: [{type: mongoose.Types.ObjectId, ref: 'user'}],
+    saved: [{type: mongoose.Types.ObjectId, ref: 'user'}]
+}, {
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('user', userSchema)
